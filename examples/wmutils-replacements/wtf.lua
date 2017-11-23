6a7810577fc55604be5fdebfa6843eee28ec4c7d
Adrian Pistol <vifino@tty.sh>
2017-11-24 00:11:51 +0100

    examples/wmutils-replacements: add wtf utility

---
+++ examples/wmutils-replacements/wtf.lua
@@ -0,0 +1,15 @@
+#!/usr/bin/env ljwm
+-- Small wmutils pfw clone using ljwm 
+
+local args = {...}
+if not args[1] then
+	print("Usage: wtf <wid>")
+	os.exit(1)
+end
+
+local xcb = require("xcb.wrapper")
+local xcbe = require("xcb.enums")
+local conn = xcb.connect()
+
+local win = conn:window(args[1])
+conn:set_input_focus(win, xcbe.input_focus.POINTER_ROOT)
